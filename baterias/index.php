d76736c77b2694766f20e0b0857af2cd97319109
rgarciaq <rgarciaq03@educarex.es>
2021-10-28 13:01:41 +0200

    fichero texto claves

---
+++ baterias/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> Codigo: <input type="text" id="codigo"></p>
+    <p> Tipo: <select name="tipo" size="1">
+        <option value="1">Litio</option>
+        <option value="2">Plomo</option>
+        <option value="3">Níquel</option>
+    </select>
+    <p> Marca: <input type="text" id="marca"></p>
+    
+</body>
+</html>
